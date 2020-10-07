9976bd5b449f8101c414cde826d9e3c8a692eff9
202000358040 <Administrator@USER-20181004VU>
2020-10-07 10:11:49 +0800

    aaa

--- aaa/src/aaa/MainClass.java
+++ aaa/src/aaa/MainClass.java
@@ -1,10 +1,10 @@
 package aaa;
 
-public class aaa {
+public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       System.out.print("i love you,china!");
+	     System.out.print("i love you,china!......");
 	}
 
 }
